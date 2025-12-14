801bd0680260e7009908a94026e6ac42fb8c4d21
eai13 <eai84199@gmail.com>
2025-12-14 16:46:02 +0300

    Завершил, осталось оформить

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,64 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QDateTime>
+#include <QTimeZone>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget* parent)
+  : QMainWindow(parent)
+  , ui(new Ui::MainWindow)
+  , _tickCount{ 0 }
+  , _timerTick{ this }
+  , _timerSync{ this }
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
+
+  connect(&_timerTick, &QTimer::timeout, this, &MainWindow::onTick);
+  connect(&_timerSync, &QTimer::timeout, this, &MainWindow::onSyncDateTime);
+  connect(ui->pbStart, &QPushButton::clicked, this, &MainWindow::startTick);
+  connect(ui->pbStop, &QPushButton::clicked, this, &MainWindow::stopTick);
+
+  _timerSync.start(500);
+
+  onSyncDateTime();
+  stopTick();
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+  delete ui;
 }
 
+void
+MainWindow::onSyncDateTime()
+{
+  ui->lblTime->setText(formattedDateTime());
+}
+
+void
+MainWindow::startTick()
+{
+  ui->pbStart->setEnabled(false);
+  ui->pbStop->setEnabled(true);
+  _timerTick.start(1000);
+}
+
+void
+MainWindow::stopTick()
+{
+  ui->pbStart->setEnabled(true);
+  ui->pbStop->setEnabled(false);
+  _timerTick.stop();
+}
+
+void
+MainWindow::onTick()
+{
+  ui->teLog->append(
+    QStringLiteral("%1\tTick %2").arg(formattedDateTime()).arg(_tickCount++));
+}
+
+QString
+MainWindow::formattedDateTime() const
+{
+  return QDateTime::currentDateTime().toString("yyyy/MM/ddThh:mm:ss");
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,21 +1,33 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#pragma once
 
 #include <QMainWindow>
+#include <QTimer>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  MainWindow(QWidget* parent = nullptr);
+  ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
+  void onSyncDateTime(void);
+  void startTick(void);
+  void stopTick(void);
+  void onTick(void);
+
+  QString formattedDateTime(void) const;
+
+  Ui::MainWindow* ui;
+
+  uint64_t _tickCount{ 0 };
+  QTimer _timerTick;
+  QTimer _timerSync;
 };
-#endif // MAINWINDOW_H
